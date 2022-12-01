1d4b0491e8d8b61d76d9845b0873334a97250032
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2022-12-01 01:46:36 +0100

    update bash script to generate pdf

--- createpdf.sh
+++ createpdf.sh
@@ -1,6 +1,25 @@
+# Load variables
+set -a
+source .env
+set +a
+
+# Name of html file
 HTMLFILE="resume.html"
+
+# Name of final PDF file
 PDFFILE="resume.pdf"
 
-pipenv run weasyprint $HTMLFILE $PDFFILE
+# Replace variables in html file
+(envsubst < $HTMLFILE) > print.html
 
+# Run Python to insert content
+python main.py
+
+# Create PDF
+weasyprint print.html $PDFFILE
+
+# Clean up
+# rm print.html
+
+# Open PDF with the system's standard pdf viewer
 open $PDFFILE
